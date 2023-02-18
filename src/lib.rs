d15fe06f00b6fd4018d996e7fd6d295445d5376f
Emerald <itsemerald@protonmail.com>
2023-02-18 23:31:28 +0000

    refactor: add demo for 1.2 and 1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 use ethers::prelude::abigen;
 
-abigen!(Seaport, "src/abi.json");
+// This works
+abigen!(Seaport11, "src/abi_1_1.json");
+
+// But these don't!
+abigen!(Seaport12, "src/abi_1_2.json");
+abigen!(Seaport13, "src/abi_1_3.json");
